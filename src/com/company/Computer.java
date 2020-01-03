77d401d7c0050f9fb079973ba285b24d73232da4
hayobabson <babalolakehinde.p@gmail.com>
2020-01-03 16:14:51 +0100

    add computer class

---
+++ src/com/company/Computer.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Computer {
+    displayModule dm;
+
+    public void setDisplayModule(displayModule dm) {
+        this.dm = dm;
+    }
+    public void display(){
+        dm.display();
+    }
+}
