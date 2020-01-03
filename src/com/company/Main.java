6d86c13d73c0499c98a2271b1724835ec3c3b5b8
hayobabson <babalolakehinde.p@gmail.com>
2020-01-03 16:16:18 +0100

    add class to implement display module interface

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,15 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+        Computer cm = new Computer();
+
+        displayModule dm = new Monitor();
+        displayModule dm1 = new Project();
+
+        cm.setDisplayModule(dm);
+        cm.display();
+        cm.setDisplayModule(dm1);
+        cm.display();
     }
 }
---
+++ src/com/company/Project.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Project implements displayModule {
+    @Override
+    public void display() {
+        System.out.println("Display through projector");
+    }
+}
